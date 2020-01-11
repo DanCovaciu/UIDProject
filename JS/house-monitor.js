a188aa8f1d066bc2cb917bfe1cbb1ac405e63096
CodreaAncuta <33701210+CodreaAncuta@users.noreply.github.com>
2020-01-11 20:57:50 +0200

    Finalize House Monitor

--- JS/house-monitor.js
+++ JS/house-monitor.js
@@ -1,12 +1,9 @@
 var oldValue = 100;
+var dummyDataInitial = ["100 kWh", "20 kWh", "90 kWh", "120 kWh", "170 kWh"];
+var dummyDataNextDay = ["199 kWh", "230 kWh", "780 kWh", "120 kWh", "70 kWh"];
+var dummyDataPreviousDay = ["567 kWh", "342 kWh", "900 kWh", "1200 kWh", "170 kWh"];
 
-jQuery(document).ready(function () {
-	// visibility of components
-	$("#solarProductionMonitor").hide();
-    $("#readInverter").hide();
-	$("#dataFeatures").hide();
-	$(".btns").show();
-	
+$(document).ready(function () {
 	// click actions
 	$('#confirm').on('click', doOnConfirm);
 	$('#cancel').on('click', doOnCancel);
@@ -18,90 +15,68 @@ jQuery(document).ready(function () {
 	$("#nextButton").on('click', view_next_day_satistics);
 	
 	$('#close').on('click', function() {
-		$(this).parent().fadeOut();
-		$(".btns").fadeIn();
+		$(this).parent().hide();
+		$("#solarHouse").show();
 	});
 	
 	$('#closeDataFeatures').on('click', function() {
-		$(this).parent().fadeOut();
-		$(".btns").fadeIn();
+		$(this).parent().hide();
+		$("#solarHouse").show();
 	});
 	
 	
 	// solar production monitoring
 	$('#closeSolarProductionMonitor').on('click', function() {
-		$(this).parent().fadeOut();
-		$(".btns").fadeIn();
+		$(this).parent().hide();
+		$("#solarHouse").show();
 	});
 	
-	var energyValuesList = $('.evalue').get();
-	console.log(energyValuesList);
-	var a = [];
-	for ( var i = 0; i < energyValuesList.length; i++ ) {
-		a.push( energyValuesList[ i ].innerHTML );
-	}
-	
-	console.log("Data");
-	console.log(a);
-	
-	var options = {
-		animationEnabled: true,
-		title: {
-			text: "Solar Production"
-		},
-		axisY: {
-			title: "Energy Values in kWh",
-			includeZero: false
-		},
-		axisX: {
-			title: "Hours"
-		},
-		data: [{
-			type: "column",
-			dataPoints: [
-				{ label: "08:00", y:  parseInt(a[0].replace(' kWh','')) },	
-				{ label: "12:00", y:  parseInt(a[1].replace(' kWh','')) },	
-				{ label: "16:00", y:  parseInt(a[2].replace(' kWh','')) },
-				{ label: "20:00", y:  parseInt(a[3].replace(' kWh','')) },
-				{ label: "00:00", y:  parseInt(a[4].replace(' kWh','')) }
-			]
-		}]
-	};
-	$("#chartContainer").CanvasJSChart(options);
-
+	// initialize the chart
+	initialize(dummyDataInitial);
 });
 
 var day, date;
 
 function open_solar_production_monitor() {
-     $(".btns").hide();
+	 $("#readInverter").hide();
+	 $("#dataFeatures").hide();
+	 $("#solarHouse").hide();
 	 $("#solarProductionMonitor").show();
-	 date = $('#currentDate').text();
 	 console.log(date);
-	 day = parseInt(date.replace(/[^0-9]/g, ''));
 }
 
 function open_read_inverter_modal() {
-     $(".btns").hide();
+	 $("#solarProductionMonitor").hide();
+	 $("#dataFeatures").hide();
+	 $("#solarHouse").hide();
 	 $("#readInverter").show();
 	 $("#oldIndex").val(oldValue);
 }
 
 function open_system_data_modal() {
-	$(".btns").hide();
+	$("#solarProductionMonitor").hide();
+	$("#readInverter").hide();
+	$("#solarHouse").hide();
 	$("#dataFeatures").show();
 }
 
 function doOnConfirm() {
-	 oldValue = $("#newIndex").val();
-	 $("#readInverter").hide();
-	 $(".btns").show();
-	 $("#newIndex").val('');
+	 if ($("#newIndex").val() == 0){
+		 $("#newIndex").css("border-color" , "red");
+	 }
+	 else {
+		oldValue = $("#newIndex").val();
+		$("#readInverter").hide();
+		$(".btns").show();
+		$("#solarHouse").show();
+		$("#newIndex").val('');
+	 }
 }
 
 function doOnCancel() {
 	 $("#readInverter").hide();
 	 $(".btns").show();
+	 $("#solarHouse").show();
 	 $("#newIndex").val('');
 }
 
@@ -109,85 +84,85 @@ var months = ["January", "February", "March", "April", "May", "June", "July", "A
 var monthDurationDays = [31, 29, 31, 30, 31, 30, 31, 31, 39, 31, 30, 31];
 
 function view_previous_day_statistics() {
-	day = day - 1;
+	day = parseInt($('#currentDate').text().replace(/[^0-9]/g, '')) - 1;
+	console.log("Now DAY: " + day);
 	
 	var newDate;
-	// check if day is 0, then first day of previous month
-	if(day == '0'){
+	// check if day is 0, then go to first day of previous month
+	if(day == 0){
 		date = $('#currentDate').text().replace(new RegExp("[0-9]","g"), "").replace(/\s/g,'');
+		console.log("Date NOW: " + date);
 		
 		// first month from the year
 		if (months.indexOf(date) == 0){
 			console.log(months);
 			console.log(months.length);
-			newDate = months[months.length - 1] + " 1";
-			day = 1;
+			newDate = months[months.length - 1] + " " + monthDurationDays[months.length - 1];
 			
 			var currentYear = parseInt($('#currentYear').text());
 			$('#currentYear').text(currentYear - 1);
+			
+			console.log("New date: " + newDate);
+			console.log("Year: " + currentYear-1);
 		}
 		else {
-			newDate = months[months.indexOf(date) - 1] + " 1";
-			day = 1;
+			newDate = months[months.indexOf(date) - 1] + " " + monthDurationDays[months.length - 1];
 		}
-		$('#currentDate').text(newDate);
 	}
 	else {
-		date = $('#currentDate').text().replace(new RegExp("[0-9]","g"), "");
-		date += day;
-		console.log("Previous date: " + date);
-		$('#currentDate').text(date);
+		newDate = $('#currentDate').text().replace(new RegExp("[0-9]","g"), "");
+		newDate += day;
 	}
+	$('#currentDate').text(newDate);
+	initialize(dummyDataPreviousDay);
 }
 
 function view_next_day_satistics() {
-	day = day + 1;
-	console.log("here day: " + day);
+	day = parseInt($('#currentDate').text().replace(/[^0-9]/g, '')) + 1;
+	console.log("NOW day: " + day);
 	
 	var newDate;
 	
 	var currentMonthName = $('#currentDate').text().replace(new RegExp("[0-9]","g"), "").replace(/\s/g,'');
 	var monthName = months[months.indexOf(currentMonthName)];
-	var monthDay = monthDurationDays[months.indexOf(currentMonthName)];
+	var monthDay = monthDurationDays[months.indexOf(currentMonthName)] + 1;
 	
-	// check if is January and day 31 then next month
+	console.log("currentMonthName: " + currentMonthName);
+	console.log("monthName: " + monthName);
+	console.log("monthDay: " + monthDay);
+	
+	// check if is the last day of the month
 	if(monthDay == day && monthName == currentMonthName){
 		// last month from the year
-		if (months.indexOf(currentMonthName) == months.length){
+		if (months.indexOf(currentMonthName) == months.length - 1){
+			console.log("last month");
 			newDate = months[0] + " 1";
-			day = 1;
 			
 			var currentYear = parseInt($('#currentYear').text());
 			$('#currentYear').text(currentYear + 1);
 		}
 		else {
-			console.log("must be here");
+			console.log("not last month");
 			newDate = months[months.indexOf(currentMonthName) + 1] + " 1";
 			console.log(newDate);
-			day = 1;
 		}
-		//$('#currentDate').text(newDate);
 	}
 	else {
 		newDate = $('#currentDate').text().replace(new RegExp("[0-9]","g"), "");
 		newDate += day;
 		console.log("Next date: " + newDate);
-		
 	}
 	$('#currentDate').text(newDate);
-	rewriteData();
+	initialize(dummyDataNextDay);
 }
 
-var dummyData = ["100 kWh", "20 kWh", "90 kWh", "120 kWh", "170 kWh"];
-
-function rewriteData() {
-	
+function initialize(data){
 	var energyValuesList = $('.evalue').get();
-	
 	console.log(energyValuesList);
+	
 	var a = [];
 	for ( var i = 0; i < energyValuesList.length; i++ ) {
-		energyValuesList[ i ].innerHTML = dummyData[ i ];
+		energyValuesList[ i ].innerHTML = data[ i ];
 		a.push( energyValuesList[ i ].innerHTML );
 	}
 	
@@ -217,5 +192,6 @@ function rewriteData() {
 			]
 		}]
 	};
+	
 	$("#chartContainer").CanvasJSChart(options);
 }
